d96c0fee59784449e0906ffba72364f7857b312b
Caleb Josue Ruiz Torres <calebjosue-dev@outlook.com>
2024-12-12 15:53:48 -0600

    master: create-queries lab migrated to JakartaEE, guided exercise implemented

--- labs/create-queries/src/main/java/com/redhat/training/model/Person.java
+++ labs/create-queries/src/main/java/com/redhat/training/model/Person.java
@@ -1,37 +1,45 @@
 package com.redhat.training.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.NamedQuery;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
 
 @Entity
-//add named query here
-
+@NamedQuery(
+    name = "getAllPersonsWithName",
+    query = "SELECT p FROM Person p WHERE p.name = :pname"
+)
 public class Person{
+
     @Id
     private Long id;
 
     private String name;
 
     public Long getId() {
+
         return id;
     }
 
     public void setId(Long id) {
+
         this.id = id;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
+
+        final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
@@ -39,20 +47,36 @@ public class Person{
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
+
+        if (this == obj) {
+
+            return true;
+        }
+
+		if (obj == null) {
+
+            return false;
+        }
+
+
+		if (getClass() != obj.getClass()) {
+
+            return false;
+        }
+
 		Person other = (Person) obj;
+
 		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
+
+			if (other.id != null) {
+
+                return false;
+            }
+		} else if (!id.equals(other.id)) {
+
+            return false;
+        }
+
 		return true;
 	}
-
-    
 }
--- labs/create-queries/src/main/java/com/redhat/training/services/PersonService.java
+++ labs/create-queries/src/main/java/com/redhat/training/services/PersonService.java
@@ -1,24 +1,35 @@
 package com.redhat.training.services;
 
-import java.util.List;
-
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.Query;
-
 import com.redhat.training.model.Person;
 
-@Stateless
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 
+import java.util.List;
+
+@Stateless
 public class PersonService {
 
-  @PersistenceContext(unitName="hello")
+    @PersistenceContext(unitName="hello")
 	private EntityManager entityManager;
 
 	// Get all Person objects in the Database
+    public List<Person> getAllPersons() {
 
-  //Get persons whose name matches the name given in the query
+        TypedQuery<Person> query = entityManager.createQuery("SELECT p FROM Person p", Person.class);
 
+        return query.getResultList();
+    }
+
+    //Get persons whose name matches the name given in the query
+    public List<Person> getPersonsWithName(String name) {
+
+        TypedQuery<Person> query = entityManager.createNamedQuery("getAllPersonsWithName", Person.class);
+
+        query.setParameter("pname", name);
+
+        return query.getResultList();
+    }
 }
