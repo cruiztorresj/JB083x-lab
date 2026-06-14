cf12f769d6837657ace5b3face147584709bf3c7
Caleb Josue Ruiz Torres <calebjosue-dev@outlook.com>
2026-06-13 22:53:13 -0600

    master: hello-rest now in Jakarta EE 11

--- labs/hello-rest/src/main/java/com/redhat/training/rest/Service.java
+++ labs/hello-rest/src/main/java/com/redhat/training/rest/Service.java
@@ -4,6 +4,4 @@ import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
-public class Service extends Application {
-
-}
+public class Service extends Application { /* ... */ }
