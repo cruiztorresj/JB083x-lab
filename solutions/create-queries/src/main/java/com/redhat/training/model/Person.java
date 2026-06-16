8ad0bc02121c9060439475c35f0353a91b012518
Caleb Josue Ruiz Torres <calebjosue-dev@outlook.com>
2026-06-16 17:27:10 -0600

    create-queries solution now in Jakarta EE 11

--- solutions/create-queries/src/main/java/com/redhat/training/model/Person.java
+++ solutions/create-queries/src/main/java/com/redhat/training/model/Person.java
@@ -1,38 +1,44 @@
 package com.redhat.training.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.NamedQuery;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
 
 @Entity
 //add named query here
 @NamedQuery(
 		name="getAllPersonsWithName",
 		query="select p from Person p where p.name = :pname")
-public class Person{
+public class Person {
+
     @Id
     private Long id;
 
     private String name;
 
     public Long getId() {
+
         return id;
     }
 
     public void setId(Long id) {
+
         this.id = id;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
 	@Override
 	public int hashCode() {
+
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
@@ -41,20 +47,31 @@ public class Person{
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Person other = (Person) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
 
-    
+		if (obj == this) {
+
+            return true;
+		} if (obj == null) {
+
+			return false;
+		} else if (obj.getClass() != getClass()) {
+
+			return false;
+        } else {
+
+            Person other = (Person) obj;
+		    if (id == null) {
+
+			    if (other.id != null) {
+
+				    return false;
+                }
+		    } else if (!id.equals(other.id)) {
+
+                return false;
+            }
+        }
+
+		return true;
+	}    
 }
