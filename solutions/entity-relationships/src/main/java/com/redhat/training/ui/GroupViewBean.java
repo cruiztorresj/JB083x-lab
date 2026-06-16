a7d80313a4d2497bad43c8a0843586f25694d7d9
Caleb Josue Ruiz Torres <calebjosue-dev@outlook.com>
2026-06-16 15:57:01 -0600

    entity-relationships now in Jakarta EE 11 faces

--- solutions/entity-relationships/src/main/java/com/redhat/training/ui/GroupViewBean.java
+++ solutions/entity-relationships/src/main/java/com/redhat/training/ui/GroupViewBean.java
@@ -44,8 +44,14 @@ public class GroupViewBean {
 	public void update(ValueChangeEvent event) {
 
 		PersonaGroup group = (PersonaGroup) event.getNewValue();
-		personas = new HashSet<Persona>(group.getPersonas());
-			
+
+        if (group == null) {
+
+            personas = new HashSet<Persona>();
+        } else {
+
+            personas = new HashSet<Persona>(group.getPersonas());
+        }
 	}
 
 	public Set<PersonaGroup> getGroups() {
